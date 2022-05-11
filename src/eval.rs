fc38ecd8df0ed995ec1b73d4b0557e2e74abd733
Saecki <tobiasschmitz2001@gmail.com>
2022-05-11 13:18:51 +0200

    fix(eval): separate x and y contexts

--- src/eval.rs
+++ src/eval.rs
@@ -1,4 +1,4 @@
-use cods::{Context, Cst, Ident, IdentSpan, Scopes, Span, Val, Stack};
+use cods::{Context, Cst, Ident, IdentSpan, Scopes, Span, Stack, Val};
 use egui::plot::Value;
 use serde::{Deserialize, Serialize};
 use strum::{Display, EnumIter, EnumString, IntoEnumIterator, IntoStaticStr};
@@ -74,36 +74,44 @@ pub struct Expr {
 }
 
 pub fn eval(expr: &Expr, data: &Data) -> anyhow::Result<Vec<Value>> {
-    let mut ctx = Context::default();
+    let mut ctx_x = Context::default();
+    let mut ctx_y = Context::default();
     for v in Var::iter() {
-        ctx.idents.push(v.into());
+        ctx_x.idents.push(v.into());
+        ctx_y.idents.push(v.into());
     }
 
-    let csts_x = parse(&mut ctx, &expr.x)?;
-    let csts_y = parse(&mut ctx, &expr.y)?;
+    let csts_x = parse(&mut ctx_x, &expr.x)?;
+    let csts_y = parse(&mut ctx_y, &expr.y)?;
 
     let var_count = Var::iter().count();
     let mut vars = Vec::with_capacity(var_count);
-    let mut scopes = Scopes::default();
+    let mut scopes_x = Scopes::default();
+    let mut scopes_y = Scopes::default();
     for (id, v) in Var::iter().enumerate() {
         let ident = IdentSpan::new(Ident(id), Span::pos(0));
-        let inner = ctx.def_var(&mut scopes, ident, cods::DataType::Float, true, false);
+        let inner = ctx_x.def_var(&mut scopes_x, ident, cods::DataType::Float, true, false);
+        ctx_y.def_var(&mut scopes_y, ident, cods::DataType::Float, true, false);
         vars.push((inner, v));
     }
-    let asts_x = ctx.check_with(&mut scopes, csts_x)?;
-    let asts_y = ctx.check_with(&mut scopes, csts_y)?;
+
+    let asts_x = ctx_x.check_with(&mut scopes_x, csts_x)?;
+    let asts_y = ctx_y.check_with(&mut scopes_y, csts_y)?;
 
     let mut values = Vec::with_capacity(data.len());
-    let mut stack = Stack::default();
-    stack.extend_to(vars.len());
+    let mut stack_x = Stack::default();
+    let mut stack_y = Stack::default();
+    stack_x.extend_to(vars.len());
+    stack_y.extend_to(vars.len());
     for e in data.iter() {
         for (var, val) in vars.iter() {
             let val = get_value(e, *val);
-            stack.set(var, val)
+            stack_x.set(var, val.clone());
+            stack_y.set(var, val);
         }
 
-        let x = cods::eval_with(&mut stack ,&asts_x);
-        let y = cods::eval_with(&mut stack ,&asts_y);
+        let x = cods::eval_with(&mut stack_x, &asts_x);
+        let y = cods::eval_with(&mut stack_y, &asts_y);
 
         if let (Ok(x), Ok(y)) = (x, y) {
             if let (Some(x), Some(y)) = (cast_float(x), cast_float(y)) {
