1558f812f8a3093b1fdb30ac348f04c1c3befd80
Saecki <tobiasschmitz2001@gmail.com>
2022-05-15 20:48:09 +0200

    feat: reenable drag and drop support

--- src/fs.rs
+++ src/fs.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 use std::io::BufReader;
 use std::path::{Path, PathBuf};
 
-use egui::{Align2, Color32, Context, Id, LayerId, Order, TextStyle};
+use egui::{Align2, Color32, Context, Id, LayerId, Order, Pos2, Rect, TextStyle, Vec2};
 use serde::{Deserialize, Serialize};
 
 use crate::app::{PlotData, WheelValues};
@@ -32,26 +32,64 @@ impl PlotApp {
                 ctx.layer_painter(LayerId::new(Order::Foreground, Id::new("file_drop_target")));
             let screen_rect = ctx.input().screen_rect();
             painter.rect_filled(screen_rect, 0.0, Color32::from_black_alpha(192));
+
+            // Draw plus
+            let w = screen_rect.width();
+            let h = screen_rect.height();
+            let center = screen_rect.center();
+
+            // Background box
+            let box_size = f32::min(w, h) * 0.04;
+            let box_rect = Rect {
+                min: Pos2::new(center.x - box_size, center.y - box_size),
+                max: Pos2::new(center.x + box_size, center.y + box_size),
+            };
+            painter.rect_filled(box_rect, box_size * 0.3, Color32::from_white_alpha(50));
+
+            // Forground
+            let long_extend = box_size * 0.6;
+            let short_extend = long_extend * 0.1;
+            let color = Color32::from_gray(0);
+            let rect = Rect {
+                min: Pos2::new(center.x - long_extend, center.y - short_extend),
+                max: Pos2::new(center.x + long_extend, center.y + short_extend),
+            };
+            painter.rect_filled(rect, 0.0, color);
+            let rect = Rect {
+                min: Pos2::new(center.x - short_extend, center.y - long_extend),
+                max: Pos2::new(center.x + short_extend, center.y + long_extend),
+            };
+            painter.rect_filled(rect, 0.0, color);
+
+            // File names
+            let pos = center + Vec2::new(0.0, box_size * 2.0);
+            let mut text = String::new();
+            for f in ctx.input().raw.hovered_files.iter() {
+                if let Some(p) = &f.path {
+                    text += &format!("\n{}", p.display());
+                }
+            }
             painter.text(
-                screen_rect.center(),
-                Align2::CENTER_CENTER,
-                "Dropping files",
-                TextStyle::Body.resolve(&ctx.style()),
-                Color32::WHITE,
+                pos,
+                Align2::CENTER_TOP,
+                text,
+                TextStyle::Heading.resolve(&ctx.style()),
+                Color32::from_white_alpha(160),
             );
         }
 
         // Collect dropped files
         if !ctx.input().raw.dropped_files.is_empty() {
-            if let Some(_p) = ctx
+            if let Some(p) = ctx
                 .input()
                 .raw
                 .dropped_files
                 .first()
                 .and_then(|f| f.path.as_ref())
             {
-                // TODO: areas for data and temperature
-                //self.try_open(p.clone());
+                if let Ok(files) = find_files(p) {
+                    self.try_open(files);
+                }
             }
         }
     }
